be115d8963967e1d08684b6dfda50df656cf06c4
qjchen1972@163.com <qjchen1972@163.com>
2022-07-24 16:39:37 +0800

    modify

--- trainer.py
+++ trainer.py
@@ -29,6 +29,7 @@ class KmeanConfig:
         for k,v in kwargs.items():
             setattr(self, k, v)
             
+
     
 class KmeanTrainer:
 
@@ -72,6 +73,14 @@ class KmeanTrainer:
                state_dict['epoch']+1, state_dict['loss']
     
     
+    '''
+    input：
+      x: 输入向量 
+      c：cluster
+    return：    
+      idx: x对应cluster的索引索引
+      loss: x - c 的距离累计             
+    '''
     @classmethod    
     def data2cluster(cls, x, c):
         if isinstance(x, list):
@@ -83,13 +92,6 @@ class KmeanTrainer:
     
     '''
     Euclidean distance
-    input：
-      x: 输入向量 
-      c：cluster
-    return：
-      x: 返回输入向量，可能会被单位化
-      idx: x对应cluster的索引索引
-      loss: x - c 的距离累计             
     '''
     @classmethod
     def data2cluster_euc(cls, x, c):
@@ -102,13 +104,6 @@ class KmeanTrainer:
      
     '''
     cosine distance
-    input：
-      x: 输入向量 
-      c：cluster
-    return：
-      x: 返回输入向量，可能会被单位化
-      idx: x对应cluster的索引索引
-      loss: x和c的cos距离和            
     '''    
     @classmethod    
     def data2cluster_cos(cls, x, c):
@@ -123,7 +118,16 @@ class KmeanTrainer:
         value, idx = temp.max(dim=1)
         return idx, value.sum()
     
-    
+     '''    
+    input：
+      x: 输入向量 
+      c：cluster
+      c_num: cluster的数量
+    return：
+      x: 返回输入向量，可能会被单位化
+      idx: x对应cluster的索引索引
+      loss: x - c 的距离累计             
+    '''    
     def dataproc(self, x, c, c_num=None):
         if self.cfg.dis_mode == 'euc':
             return self.dataproc_euc(x, c, c_num)
@@ -135,7 +139,8 @@ class KmeanTrainer:
             value, idx = data.max(dim=1)
         else:
             value, idx = data.min(dim=1)
-        return  idx, value           
+        return  idx, value       
+        
         #选择最可能的10个值, 从这些值里选满足阈值的值，然后优先分配给数目最大的cluster
         value, idx = torch.topk(data, 
                                 k=self.cfg.top_select, 
@@ -161,7 +166,7 @@ class KmeanTrainer:
         ans_value = torch.tensor(ans_value, dtype=torch.float32)
         return ans_idx, ans_value
         
-    
+    #Euclidean distance
     def dataproc_euc(self, x, c, c_num):
     
         temp = x[:, None, :] - c[None, :, :]
@@ -171,6 +176,7 @@ class KmeanTrainer:
         
         return x, ans_idx, ans_value.sum()
     
+    #cos distance
     def dataproc_cos(self, x, c, c_num):
         #转为单位向量
         x = x / torch.clamp(torch.norm(x, 2, dim=1)[:, None], 
@@ -204,6 +210,7 @@ class KmeanTrainer:
             new_cluster_num = torch.zeros(c.shape[0]).to(device)
             
             for it, x in pbar:
+                #if it == 1: exit()            
                 x = x.to(device)
                 with torch.set_grad_enabled(False):
                     #若是cos距离，需要把x转为单位向量，来求出新的cluster
@@ -213,6 +220,7 @@ class KmeanTrainer:
                         t = x[idx==k]
                         new_cluster_num[k] += t.shape[0]
                         new_cluster[k] += t.sum(dim=0)
+                #if it == 1: exit()        
                 pbar.set_description(f"epoch {epoch} iter {it}: train loss {loss.item():.5f}")
             logger.info('train loss = %.3f' %(sum(total_loss))) 
             new_cluster =  new_cluster / new_cluster_num[:, None]        
